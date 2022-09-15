d186622a5e9ebe50a7240969d4c1b0f41c92ae9f
michaelgetachew <mikygetyos@gmail.com>
2022-09-16 02:08:27 +0300

    Feat(add): added the separate data for testing and training

--- scripts/cleaner.py
+++ scripts/cleaner.py
@@ -1,3 +1,6 @@
+from cgitb import text
+from turtle import pos
+from typing_extensions import Self
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import Normalizer, MinMaxScaler, StandardScaler
@@ -7,11 +10,18 @@ from sklearn.preprocessing import LabelEncoder
 from gensim.parsing.preprocessing import remove_stopwords
 import re
 
+import nltk
+from nltk.stem import WordNetLemmatizer, PorterStemmer
+
 class DataCleaner:
     def __init__(self) -> None:
         """Initilize class."""
         try:
-            pass
+            
+            nltk.download('wordnet')
+            nltk.download('stopwords')
+            nltk.download('omw-1.4')
+            
             self.logger = log("data_cleaner.log").get_app_logger()
             self.logger.info(
                 'Successfully initialized data cleaner Object')
@@ -94,6 +104,18 @@ class DataCleaner:
 
         return round(totalMising / totalCells * 100, 2)
 
+    def get_numerical_columns(self, df: pd.DataFrame) -> list:
+        """
+        get numerical columns
+        """
+        return df.select_dtypes(include=['number']).columns.to_list()
+
+    def get_categorical_columns(self, df: pd.DataFrame) -> list:
+        """
+        get categorical columns
+        """
+        return  df.select_dtypes(include=['object','datetime64[ns]']).columns.to_list()
+
     def percent_missing_column(self, df: pd.DataFrame, columns:list) -> pd.DataFrame:
         """
         calculate the percentage of missing values for the specified column
@@ -109,7 +131,74 @@ class DataCleaner:
                 rows.append([col,"Not found","Not found","Not found","Not found"])
         return pd.DataFrame(data=rows,columns=["Col Name","Total","Missing","%","Data Type"]).sort_values(by="%",ascending=False)
 
+            
 
+    
+    def fill_missing_values_categorical(self, df: pd.DataFrame, method: str,columns:list=[]) -> pd.DataFrame:
+        """
+        fill missing values with specified method
+        """
+
+        if len(columns)==0:
+            columns = df.select_dtypes(include=['object','datetime64[ns]']).columns
+
+
+        if method == "ffill":
+
+            for col in columns:
+                df[col] = df[col].fillna(method='ffill')
+
+            self.logger.info(f'fill missing values using ffill')
+
+            return df
+
+        elif method == "bfill":
+
+            for col in columns:
+                df[col] = df[col].fillna(method='bfill')
+            
+            self.logger.info(f'fill missing values using bfill')
+
+            return df
+
+        elif method == "mode":
+            
+            for col in columns:
+                df[col] = df[col].fillna(df[col].mode()[0])
+            self.logger.info(f'fill missing values using mode')
+
+            return df
+        else:
+            print("Method unknown")
+            self.logger.error(f'failed to fill missing values; method unkown')
+
+            return df
+
+    def fill_missing_values_numeric(self, df: pd.DataFrame, method: str,columns: list =None) -> pd.DataFrame:
+        """
+        fill missing values with specified method
+        """
+        if(columns==None):
+            numeric_columns = self.get_numerical_columns(df)
+        else:
+            numeric_columns=columns
+
+        if method == "mean":
+            for col in numeric_columns:
+                df[col].fillna(df[col].mean(), inplace=True)
+            self.logger.info(f'fill missing values by mean')
+
+
+        elif method == "median":
+            for col in numeric_columns:
+                df[col].fillna(df[col].median(), inplace=True)
+            self.logger.info(f'fill missing values by median')
+
+        else:
+            print("Method unknown")
+            self.logger.error(f'failed to fill missing values; method unkown')
+        
+        return df
 
     def remove_nan_categorical(self, df: pd.DataFrame) -> pd.DataFrame:
         """
@@ -199,27 +288,99 @@ class DataCleaner:
             df[feature] = encodder.transform(df[feature])
         return df
 
-    def clean_links(self,df:pd.DataFrame,columns:list)->pd.DataFrame:
-        df2=df.copy()
+    def clean_links(self,df:pd.DataFrame,columns:list=[])->pd.DataFrame:
+        if len(columns) == 0:
+            columns=self.get_categorical_columns(df)
         for col in columns:
-            df2[col]=df[col].apply(lambda x:re.sub(r"\S*https?:\S*", "",x))
-            df2[col]=df[col].apply(lambda x:re.sub(r"\S*www.\S*", "", x))
+            try:
+                df[col]=df[col].apply(lambda x:re.sub(r"\S*https?:\S*", "",x))
+                df[col]=df[col].apply(lambda x:re.sub(r"\S*www.\S*", "", x))
+            except:
+                pass
             
-        return df2
+        return df
 
-    def clean_symbols(self,df:pd.DataFrame,columns:list)->pd.DataFrame:
-        df2=df.copy()
-        df2[columns].replace(to_replace=r'/',value=" ",regex=True,inplace=True)
-        for col in columns:
-            df2[col]=df2[col].replace(to_replace= r'\\', value= '', regex=True)
-            df2[col]=df2[col].replace(to_replace= r'[^\w]', value= ' ', regex=True)
-            df2[col]=df2[col].replace(to_replace= r'\u2026', value= ' ', regex=True)
-            df2[col]=df2[col].replace(to_replace=r'/',value=" ",regex=True)
-        return df2
-   
+    def clean_symbols(self,df:pd.DataFrame,columns:list=[])->pd.DataFrame:
 
-    def clean_stopwords(self,df:pd.DataFrame,columns:list):
-        df2=df.copy()
+        if len(columns) == 0:
+            columns=self.get_categorical_columns(df)
+
         for col in columns:
-            df2[col]=df2[col].apply(lambda x:remove_stopwords(x))
-        return df2
+            df[col]=df[col].apply(lambda x:re.sub(r'[^\w]', ' ',x))
+        return df
+
+    def clean_stopwords(self,df:pd.DataFrame,columns:list = []):
+         
+        if len(columns) == 0:
+            columns=self.get_categorical_columns(df)
+
+        for col in columns:
+            df[col]=df[col].apply(lambda x:remove_stopwords(x))
+        return df
+
+    def convert_to_lower_case(self,df:pd.DataFrame,columns:list = []):
+        columns=self.get_categorical_columns(df)
+        for col in columns:
+            df[col]=df[col].apply(lambda x: str(x).lower())
+        return df
+
+    def stem_word(self,df:pd.DataFrame,columns:list=[]):
+        
+        stemmer = PorterStemmer()
+        
+        def stem(txt:str):
+            stemmed_words = []
+            try:
+                word_list = nltk.word_tokenize(txt)
+            except:
+                word_list=txt.split()
+            
+            for word in word_list:
+                try:
+                    stemmed_words.append(stemmer.stem(word)) 
+                except:
+                    stemmed_words.append(word)
+            return ' '.join(stemmed_words)
+
+        if len(columns) == 0:
+            columns = self.get_categorical_columns(df)
+
+        for col in columns:
+            df[col] = df[col].apply(lambda x:stem(x))
+        return df
+
+        
+
+    def lemantize(self,df:pd.DataFrame,columns:list=[]):
+
+        wordnet_lemmatizer = WordNetLemmatizer()
+
+        def leman(txt:str):
+            leman_words = []
+            try:
+                word_list = nltk.word_tokenize(txt)
+            except:
+                word_list=txt.split()
+            
+            for word in word_list:
+                try:
+                    leman_words.append(wordnet_lemmatizer.lemmatize(word,pos='v'))
+                except:
+                    pass
+            return ' '.join(leman_words)
+
+        if len(columns) == 0:
+            columns = self.get_categorical_columns(df)
+
+        for col in columns:
+            df[col] = df[col].apply(lambda x:leman(x))
+        return df
+    
+    def trail_space_remove(self,df:pd.DataFrame,columns:list=[]):
+
+        if len(columns) is 0:
+            columns = self.get_categorical_columns(df)
+
+        for col in columns:
+            df[col] = df[col].apply(lambda x:re.sub(r' +', ' ',x))
+        return df
