659d15fc61829486677136ff0f32f18c1ba21c15
michaelgetachew <mikygetyos@gmail.com>
2022-09-15 10:58:24 +0300

    Feat(add): great modifications to the code

--- scripts/log_creator.py
+++ scripts/log_creator.py
@@ -1,31 +1,13 @@
-from cgitb import handler
-import os
 import logging
-# A Function to create and save logs in the required format
+import os
 
-def log(path, file):
+
+
+class log:
     
-    # check if the file exists
-    log_file = os.path.join(path, file)
+    def __init__(self, file_name: str, level=logging.INFO):
+        path = "../logs"
+        isExist = os.path.exists(path)
 
-    if not os.path.isfile(log_file):
-        open(log_file, 'w+').close()
-
-    console_logging_format = "%(levelname)s %(message)s"
-    file_logging_format = "%(asctime)s : %(levelname)s: %(name)s: %(module)s: %(funcName)s: %(message)s"
-
-    # Configuring the logger
-    logging.basicConfig(level=logging.INFO, format=console_logging_format)
-    logger = logging.getLogger()
-
-    # Create a file handler for the output file
-    handler = logging.FileHandler(log_file)
-
-    # Create Logging level
-    handler.setLevel(logging.INFO)
-
-    # Create a logging format
-    formatter = logging.Formatter(file_logging_format)
-    handler.setFormatter(formatter)
-
-    return logger
+        if not isExist: 
+            os.makedirs(path)
